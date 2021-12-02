f89b576ebdfdba72b17d471f5b76b1c7a1258ade
caohb <caohb1915@gmail.com>
2021-12-02 21:12:59 +0800

    修改upgrader.run代码，保证upgrader screeps能够正常工作

--- src/modules/role.upgrader.js
+++ src/modules/role.upgrader.js
@@ -2,17 +2,28 @@ export let roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.store.getFreeCapacity() > 0) {
-            let sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }
+
+        if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.upgrading = false;
+            creep.say('🔄 harvest');
         }
-        else {
+        if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
+            creep.memory.upgrading = true;
+            creep.say('🚧 upgrade');
+        }
+
+        if(creep.memory.upgrading) {
             if( creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE ) {
                 creep.moveTo(creep.room.controller);
             }
         }
+        else {
+            let sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
+
     },
 
     create: function() {
